6fe14d99e5e9e7a9eab2f395c88993da809b78ba
Luuk W. (Twinone) <twinonedevs@gmail.com>
2017-08-04 00:53:46 +0200

    Broadcast

--- backend/main.go
+++ backend/main.go
@@ -11,34 +11,43 @@ import (
 )
 
 const Path = "/echo"
+
 var addr = flag.String("addr", ":8080", "http service address")
+
+var conns map[*websocket.Conn]bool
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
+func broadcast(msg string) {
+	for conn := range (conns) {
+		conn.WriteMessage(websocket.TextMessage, []byte(msg))
+	}
+}
+
 func echo(w http.ResponseWriter, r *http.Request) {
-	c, err := upgrader.Upgrade(w, r, nil)
+	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Print("upgrade:", err)
 		return
 	}
 
 	log.Println("Client connected")
-	defer c.Close()
-	for {
-		mt, message, err := c.ReadMessage()
-		if err != nil {
-			log.Println("read:", err)
-			break
+	defer func() {
+		conn.Close()
+		delete(conns, conn)
+	}()
+	conns[conn] = true
+
+	// reader
+	go func() {
+		for {
+			_, message, _ := conn.ReadMessage()
+			log.Printf("recv: %s", message)
+			broadcast(string(message))
 		}
-		log.Printf("recv: %s", message)
-		err = c.WriteMessage(mt, message)
-		if err != nil {
-			log.Println("write:", err)
-			break
-		}
-	}
+	}()
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -46,6 +55,8 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	conns = make(map[*websocket.Conn]bool)
+
 	flag.Parse()
 	log.SetFlags(0)
 	http.HandleFunc(Path, echo)
