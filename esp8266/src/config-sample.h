62c4b0c20f312e86d2a34b3d21ddb257ea33c599
Luuk W. (Twinone) <twinonedevs@gmail.com>
2017-09-25 10:55:06 +0200

    Don't accidentally commit config.h

---
+++ esp8266/src/config-sample.h
@@ -0,0 +1,29 @@
+// Edit this file and save it as config.h
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+// This whole file can be replaced by a captive
+// portal, and the ID can be generated on the
+// backend, I'll leave this for later, so for now
+// just enter your details here
+
+// Your wifi details
+#define WIFI_SSID ""
+#define WIFI_PASS ""
+
+// Your google account
+#define ADMIN_ACCOUNT ""
+// Random 40 byte hex string
+#define BOARD_ID ""
+
+// Change to use your own server
+#define REMOTE_ADDR "iot.twinone.xyz"
+#define REMOTE_URL "/echo"
+#define REMOTE_PORT 443
+#define USE_SSL true
+
+
+
+
+
+#endif /* __CONFIG_H__ */
