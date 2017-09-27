0a37312c01a3f093aaa90ebef4339e9b2c33d315
Luuk W. (Twinone) <twinonedevs@gmail.com>
2017-09-27 13:30:10 +0200

    Ignore node modules

--- watcher.sh
+++ watcher.sh
@@ -3,10 +3,11 @@
 DIR=./backend
 REMOTEDIR=/root/go/src/github.com/twinone/iot
 while :; do
+	rsync -r --exclude "*node_modules*" --exclude "*BuildRoot*" . s:$REMOTEDIR
+	echo "Synced"
+
 	fswatch -1 $DIR >/dev/null 2>&1
 	echo -n ".";
 	sleep 1;
 	echo -n ".."
-	rsync -r . s:$REMOTEDIR
-	echo "Synced"
 done
